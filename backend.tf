c72a1585dfecf829c1f2ad0dff8f07c0adf1c9db
kato <luzazul86@gmail.com>
2022-02-05 20:28:39 +0900

    Add s3 public-access-block settings

--- backend.tf
+++ backend.tf
@@ -16,10 +16,24 @@ resource "aws_s3_bucket" "terraform-state" {
     }
   }
 
+  # バージョニングを設定
+  versioning {
+    enabled = true
+  }
+
   tags = {
     Terraform = "true"
     Name      = "terraform"
   }
+}
+
+# S3 パブリックアクセスをブロック
+resource "aws_s3_bucket_public_access_block" "terraform-state" {
+  bucket                  = aws_s3_bucket.terraform-state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 # DynamoDB
